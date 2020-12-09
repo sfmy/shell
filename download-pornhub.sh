d2bd5f14407d5750b73672e9717221ab3c4c42ab
sfmy <linye.cao.cn@gmail.com>
2020-12-09 22:58:45 +0800

    sfmy

--- download-pornhub.sh
+++ download-pornhub.sh
@@ -5,9 +5,9 @@ JSONNAME="videos.json"
 # @param1 json '{ "keyword": "final fantasy", "filter": "final fantasy", "hd": 1 }'
 function search () {
 	keyword=`echo $1 | jq -r .keyword | sed 's/-/+/g' | sed 's/ /+/g'`
-	[[ -n `echo $1 | jq .hd` ]] && keyword=$keyword"&hd=1"
+	[[ `echo $1 | jq .hd` = 1 ]] && keyword=$keyword"&hd=1"
 	filter=`echo $1 | jq -r .filter`
-	filter=${filter:-".*"}
+	filter=${filter:- ".*"}
 	touch $JSONNAME
 	[[ `cat $JSONNAME | wc -l` -eq 0 ]] && printf "{\n}" >> $JSONNAME
 	gsed -i '$d' $JSONNAME # delete last line }
@@ -16,23 +16,23 @@ function search () {
 		pageurl="https://cn.pornhub.com/video/search?search=$keyword&page=$i"
 		echo "parsing $pageurl"
 		proxychains4 -q curl -s $pageurl | \
-			grep -E "\"duration\"|view_video.*title.*class=\"\"|\"views\"|\"value\"" \
+			grep -E "\"duration\"|videoPreviewBg|\"views\"|\"value\"" \
 			> tmp
 
-		url=""; title=""; duration=""; views=""
+		url=""; title=""; duration=""; views=""; value=""
 		cat tmp | \
-			grep -i "$filter" -A 2 -B 1 | \
-			grep "view_video" -A 2 -B 1 | \
+			grep -i "$filter" -A 3 | \
+			grep "view_video" -A 3 | \
 			while read item; do
-				[[ -n `echo $item | grep "duration" | grep -o -E "\d*:\d*|\d*:\d*:\d*"` ]] && 
-				       	duration=`echo $item | grep "duration" | grep -o -E "\d*:\d*|\d*:\d*:\d*"`
-				[[ -n `echo $item | grep -o "/view_video[^\"]*"` && -n $duration ]] && \
+				[[ -z $url && -n `echo $item | grep -o "/view_video[^\"]*"` ]] && \
 				       	url=`echo $item | grep -o "/view_video[^\"]*"`
-				[[ -n `echo $item | grep -o "title=\"[^\"]*" | sed 's/title="//'` && -n $duration ]] && \
+				[[ -z $title && -n `echo $item | grep -o "title=\"[^\"]*" | sed 's/title="//'` ]] && \
 				       	title=`echo $item | grep -o "title=\"[^\"]*" | sed 's/title="//'`
-				[[ -n `echo $item | grep -o "\d\+\.\d\+[KMG]*"` && -n $url ]] && \
-					views=`echo $item | grep -o "\d\+\.\d\+[KMG]*"`
-				[[ -n `echo $item | grep -o "\d\+%" | sed 's/%//'` && -n $views ]] && \
+				[[ -n $url && -z $duration && -n `echo $item | grep "duration" | grep -o -E "\d*:\d*|\d*:\d*:\d*"` ]] && 
+				       	duration=`echo $item | grep "duration" | grep -o -E "\d*:\d*|\d*:\d*:\d*"`
+				[[ -n $duration && -z $views && -n `echo $item | grep "views" | grep -o "\d*\.*\d*[KMGkmg]"` && -n $url ]] && \
+					views=`echo $item | grep -o "\d*\.*\d*[KMGkmg]"`
+				[[ -n $views && -z $value && -n `echo $item | grep -o "\d\+%" | sed 's/%//'` && -n $views ]] && \
 					value=`echo $item | grep -o "\d\+%" | sed 's/%//'`
 				if [[ -n $url && -n $title && -n $duration && -n $views  && -n $value ]]; then
 					viewkey=${url#*=}
@@ -68,8 +68,10 @@ function downitem () {
 	else
 		url="https://cn.pornhub.com"`jq -r ".\"$key\".url" $JSONNAME`
 		echo "downloading $url"
-		proxychains4 -q youtube-dl -f best -c "$url" -o "$key.mp4"  --external-downloader aria2c --external-downloader-args "-x 15"
-		if [[ $? -eq 0 ]]; then
+		if [[ ! -f "$key.mp4" ]]; then
+			proxychains4 -q youtube-dl -f best -c "$url" -o "$key.mp4"  --external-downloader aria2c --external-downloader-args "-x 15"
+		fi
+		if [[ -f "$key.mp4" ]]; then
 			lineno=`gsed -n "/\"$key\"/=" $JSONNAME`
 			lineno=$((lineno+6))
 			gsed -i "${lineno}s/false/true/" $JSONNAME
@@ -104,48 +106,59 @@ function gettm () {
 function verify () {
 	filter=$1
 	item=$2
-	result="false"
-	if [[ -n `echo $filter | jq -r .views` ]]; then
+	result="true"
+	if [[ `echo $filter | jq -r .views` != "null" ]]; then
 		num1=$(getnum `echo $filter | jq -r .views`)
 		num2=$(getnum `echo $item | jq -r .views`)
 		if [[ `echo "$num1 > $num2" | bc` -eq 1 ]]; then
 			result="false"
 		fi
 	fi
-	if [[ -n `echo $filter | jq -r .value` ]]; then
+	if [[ `echo $filter | jq -r .value` != null ]]; then
 		num1=`echo $filter | jq -r .value`
 		num2=`echo $item | jq -r .value`
 		if [[ $num1 -gt $num2 ]]; then
 			result="false"
 		fi
 	fi
-	if [[ -n `echo $filter | jq -r .min_duration` ]]; then
+	if [[ `echo $filter | jq -r .min_duration` != null ]]; then
 		num1=$(gettm `echo $filter | jq -r .min_duration`)
 		num2=$(gettm `echo $item | jq -r .duration`)
 		if [[ `echo "$num1 > $num2" | bc` -eq 1 ]]; then
 			result="false"
 		fi
 	fi
-	if [[ -n `echo $filter | jq -r .max_duration` ]]; then
+	if [[ `echo $filter | jq -r .max_duration` != null ]]; then
 		num1=$(gettm `echo $filter | jq -r .max_duration`)
 		num2=$(gettm `echo $item | jq -r .duration`)
 		if [[ `echo "$num1 < $num2" | bc` -eq 1 ]]; then
 			result="false"
 		fi
 	fi
-	echo "true"
+	echo $result
 }
 
 function download () {
-	filter=$1
+	filter=${1:- ""}
 	keys=(`jq -r keys[] $JSONNAME`)
 	for ((i=0; i<${#keys[@]}; ++i)); do
 		item=`jq -r .\"${keys[$i]}\" $JSONNAME`
-		if [[ `verify "$filter" "$item"` = "true" ]]; then
+		if [[ -z $filter || `verify "$filter" "$item"` = "true" ]]; then
+			echo "download item ${keys[$i]}"
 			downitem ${keys[$i]} 
+		else
+			echo "jump item ${keys[$i]}"
 		fi
 	done
 }
 
-# search '{ "keyword": "final fantasy", "hd": 1, "filter": "final fantasy" }'
-download '{ "views": "20K", "value": 85 }'
+if [[ $1 = "stop" ]]; then
+	num=`ps aux | grep download-porn | grep -v "grep" | awk '{print $2}'`
+	[[ -n $num ]] && kill $num
+	pkill youtube-dl
+	pkill aria2c
+else
+	search '{ "keyword": "s-cute japan", "filter": "s-cute" }'
+	download '{ "views": "500K", "min_duration": "4:00", "value": 70 }'
+fi
+
